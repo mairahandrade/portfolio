e5f720cfb4633f325aaad06d7ee050c543925073
Maíra <142888825+mairahandrade@users.noreply.github.com>
2024-03-07 05:52:50 -0300

    Update api.js

--- asserts/js/api.js
+++ asserts/js/api.js
@@ -1,26 +1,7 @@
-// Selecionando todos os elementos .acordeon
-const acordeons = document.querySelectorAll('.acordeon');
 
-// Iterando sobre cada elemento .acordeon
-acordeons.forEach(acordeon => {
-    // Selecionando o elemento de gatilho dentro de cada .acordeon
-    const trigger = acordeon.querySelector('.trigger');
-
-    // Selecionando o elemento de conteúdo dentro de cada .acordeon
-    const content = acordeon.querySelector('.content');
-
-    // Adicionando um evento de clique ao elemento de gatilho
-    trigger.addEventListener('click', () => {
-        // Alternando a classe .open no elemento .acordeon
-        acordeon.classList.toggle('open');
-
-        // Verificando se o acordeon está aberto
-        if (acordeon.classList.contains('open')) {
-            // Se estiver aberto, mostra o conteúdo
-            content.style.display = 'inline-block';
-        } else {
-            // Se estiver fechado, esconde o conteúdo
-            content.style.display = 'none';
-        }
-    });
-});
+async function fetchProfileData() {
+    const url = 'https://raw.githubusercontent.com/mairahandrade/js-developer-portfolio/main/data/profile.json';
+    const response = await fetch(url)
+    const profileData = await response.json()
+    return profileData
+}
