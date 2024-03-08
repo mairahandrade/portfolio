0e9ce624de87525dbcfb48aefb597cd66bded7ce
mairahandrade <mairahandrade@gmail.com>
2024-03-08 05:54:14 -0300

    Alteração asserts

--- asserts/js/acordeon.js
+++ asserts/js/acordeon.js
@@ -1,26 +1,15 @@
-// Selecionando todos os elementos .acordeon
-const acordeons = document.querySelectorAll('.acordeon');
 
-// Iterando sobre cada elemento .acordeon
-acordeons.forEach(acordeon => {
-    // Selecionando o elemento de gatilho dentro de cada .acordeon
-    const trigger = acordeon.querySelector('.trigger');
+const acordeonTriggers = document.querySelectorAll('.acordeon .trigger')
 
-    // Selecionando o elemento de conteúdo dentro de cada .acordeon
-    const content = acordeon.querySelector('.content');
+acordeonTriggers.forEach((trigger) => {
+    trigger.addEventListener('click', (e) => {
+        const acordeon = trigger.parentElement
+        const isOpen = acordeon.classList.contains('open')
 
-    // Adicionando um evento de clique ao elemento de gatilho
-    trigger.addEventListener('click', () => {
-        // Alternando a classe .open no elemento .acordeon
-        acordeon.classList.toggle('open');
-
-        // Verificando se o acordeon está aberto
-        if (acordeon.classList.contains('open')) {
-            // Se estiver aberto, mostra o conteúdo
-            content.style.display = 'inline-block';
+        if (isOpen) {
+            acordeon.classList.remove('open')
         } else {
-            // Se estiver fechado, esconde o conteúdo
-            content.style.display = 'none';
+            acordeon.classList.add('open')
         }
-    });
-});
+    })
+})
