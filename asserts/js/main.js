4916cc4b5102cc6e6aec94df7e7fcfa26585b474
mairahandrade <mairahandrade@gmail.com>
2024-03-08 06:41:00 -0300

    Alteração css

--- asserts/js/main.js
+++ asserts/js/main.js
@@ -1,26 +1,19 @@
 
 function updateProfileInfo(profileData) {
-    const photo = document.getElementById('profile.photo')
-    photo.src = profileData.photo
-    photo.alt = profileData.name
+       const profile = document.getElementById('profile');
+        profile.querySelector('.photo').src = profileData.photo;
+        profile.querySelector('.photo').alt = profileData.name;
+        profile.querySelector('.name').innerText = profileData.name;
+        profile.querySelector('.job').innerText = profileData.job;
+        profile.querySelector('.location').innerText = profileData.location;
+        const phone = profile.querySelector('.phone');
+        phone.innerText = profileData.phone;
+        phone.href = `tel:${profileData.phone}`;
+        const email = profile.querySelector('.email');
+        email.innerText = profileData.email;
+        email.href = `mailto:${profileData.email}`;
+    }
 
-    const name = document.getElementById('profile.name')
-    name.innerText = profileData.name
-
-    const job = document.getElementById('profile.job')
-    job.innerText = profileData.job
-
-    const location = document.getElementById('profile.location')
-    location.innerText = profileData.location
-
-    const phone = document.getElementById('profile.phone')
-    phone.innerText = profileData.phone
-    phone.href = `tel:${profileData.phone}`
-
-    const email = document.getElementById('profile.email')
-    email.innerText = profileData.email
-    email.href = `mailto:${profileData.email}`
-}
 
 function updateSoftSkills(profileData) {
     const softSkills = document.getElementById('profile.skills.softSkills')
